b1d0a2cb8669af040345314e39a9562e745fd289
indreshmishra566-stack <indreshmishra566@gmail.com>
2026-02-16 23:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
+import uuid
 
-    # select format
+app = Flask(__name__)
+
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/download", methods=["POST"])
+def download():
+
+    url = request.form.get("url")
+    quality = request.form.get("quality")
+
+    if not url:
+        return "URL missing"
+
+
+    filename = f"{DOWNLOAD_FOLDER}/{uuid.uuid4()}.%(ext)s"
+
+
     if quality == "mp3":
 
         format_code = "bestaudio/best"
@@ -17,6 +44,7 @@
         format_code = "best"
 
 
+
     ydl_opts = {
 
         "format": format_code,
@@ -27,27 +55,31 @@
 
         "merge_output_format": "mp4",
 
-        # bot bypass fix
-        "extractor_args": {
-            "youtube": {
-                "player_client": ["android_creator"]
-            }
-        },
+        # ⭐ MOST IMPORTANT LINE ⭐
+        "cookiefile": "cookies.txt",
+
+        "nocheckcertificate": True,
 
         "http_headers": {
-            "User-Agent":
-            "com.google.android.youtube/17.31.35 (Linux; Android 11)"
+
+            "User-Agent": "Mozilla/5.0"
+
         }
+
     }
 
 
-    # mp3 convert
+
     if quality == "mp3":
 
         ydl_opts["postprocessors"] = [{
+
             "key": "FFmpegExtractAudio",
+
             "preferredcodec": "mp3"
+
         }]
+
 
 
     try:
@@ -66,12 +98,13 @@
         return send_file(filepath, as_attachment=True)
 
 
+
     except Exception as e:
 
-        return f"Error: {str(e)}"
+        return f"Download failed: {str(e)}"
 
 
-# render port fix
+
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
@@ -79,4 +112,5 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
 
   
-    
+
+       
