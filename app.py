6752eb7a77e1bf71c7ed0ca79244bfa5d504c079
indreshmishra566-stack <indreshmishra566@gmail.com>
2026-02-16 23:26:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask, render_template, request, send_file
 import yt_dlp
@@ -6,7 +7,6 @@ import uuid
 app = Flask(__name__)
 
 DOWNLOAD_FOLDER = "downloads"
-COOKIE_FILE = "cookies.txt"
 
 if not os.path.exists(DOWNLOAD_FOLDER):
     os.makedirs(DOWNLOAD_FOLDER)
@@ -21,7 +21,6 @@ def index():
 def download():
 
     url = request.form["url"]
-    quality = request.form.get("quality", "best")
 
     try:
 
@@ -29,22 +28,16 @@ def download():
         filepath = os.path.join(DOWNLOAD_FOLDER, filename)
 
         ydl_opts = {
-            'format': quality,
+
+            'format': 'best',
+
             'outtmpl': filepath,
 
-            # IMPORTANT
-            'cookiefile': COOKIE_FILE,
-
-            # EXTRA FIX
             'nocheckcertificate': True,
-            'ignoreerrors': False,
-            'quiet': False,
-            'no_warnings': False,
 
-            # CRITICAL FIX
             'extractor_args': {
                 'youtube': {
-                    'player_client': ['android', 'web']
+                    'player_client': ['android']
                 }
             }
         }
@@ -55,13 +48,12 @@ def download():
         return send_file(filepath, as_attachment=True)
 
     except Exception as e:
+
         return f"Error: {str(e)}"
 
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 10000))
+
     app.run(host="0.0.0.0", port=port)
-
-
-  
-  
